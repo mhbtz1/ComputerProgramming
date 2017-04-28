ef4c223b44fa54937dc1ff1b172c734fc276f964
SirFrancisOfTheFilth <mtarekegn143@gmail.com>
2017-04-27 20:19:30 -0400

    Add files via upload

--- Vectors.cpp
+++ Vectors.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 using namespace std;
 
+//HammingDistance
 int main() {
-    float arr[5];
+  string arr[] = {"G","A","G","C","C","T","A","C","T","A","A","C","G","G","G","A","T"};
+  string arr2[] = {"C","A","T","C","G","T","A","A","T","G","A","C","G","G","C","C","T"};
 
-
-    cout << "Enter 5 numbers: ";
-    for (int i = 0; i < 5; ++i) {
-        cin >> *(arr + i) ;
+  int a = 0;
+  int i = 0;
+  while(i < sizeof(arr)/sizeof(arr[0])){
+    if(arr[i] == arr2[i]){
+      a += 1;
+      i += 1;
+    } else {
+      i += 1;
     }
-
-    cout << "Displaying data: " << endl;
-    for (int i = 0; i < 5; ++i) {
-        cout << *(arr + i) << endl ;
-    }
-
-    return 0;
+  }
+  cout << sizeof(arr)/sizeof(arr[0]) - a << endl;
+  return 0;
 }
